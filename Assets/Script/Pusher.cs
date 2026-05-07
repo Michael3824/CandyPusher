14b5e15ea6f325d0eea29d20d8bee59b3562f0cb
hoshino1002 <xingyezhen83@gmail.com>
2026-05-07 12:18:57 +0900

    unityデータの追加

---
+++ Assets/Script/Pusher.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    public float speed = 1f;
+    public float movepower = 5f;
+        private Vector3 startPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()  
+    {
+        startPosition = this.transform.position;
+        Debug.Log("ÉQÅ[ÉÄÇ™äJénÇµÇΩÇÊ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
