adda8331b64777ed5c9f059fef7cb00f633b971c
Michael3824 <TTC2650SG0048@edu.tech.ac.jp>
2026-06-18 11:43:26 +0900

    スコアとUIを作った

--- Assets/Script/StageOut.cs
+++ Assets/Script/StageOut.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StageOut : MonoBehaviour
+
 {
     // 来週の頭(一限)に適当に選んだ３人を当てる予定
     // 要件定義：StageOutクラスの中に変数 Scoreを作成、オブジェクトがすり抜けたらScoreを1加算する
     // 1.StageOutクラスの中に変数　Scoreを作成
+    private int Score;
+    public Text scoreText;
     // 2.変数 Score は int型　かつ　private であること
     // 3.オブジェクトがすり抜けたら(OutTriggerEnterが呼ばれたら)変数 Scoreに1加算する
+
     // 4.加算後の変数 ScoreをDebug.Logでコンソール上に出力する
-    
+
     //このコードがアタッチされているオブジェクトのisTrigger(コライダー設定)が有効
     //かつ他のオブジェクトがすり抜けた時に中の処理を行うイベント関数
     void OnTriggerEnter(Collider other)
     {
+        //Score = Score + 1;
+        Score += 1;
+        Debug.Log($"Score:{Score}");
+        scoreText.text = $"Score:{Score}";
         //変数名 otherってなに？
         //A.すり抜けた相手のコライダー情報
         Debug.Log($"{other.name}がすり抜けました。");
